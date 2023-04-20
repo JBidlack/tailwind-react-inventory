46c384dbdca96f3ed00c640458b5f974cb4e4230
JBidlack <john.l.bidlack@gmail.com>
2023-04-20 17:59:42 -0400

    .

--- Inventory/public/src/components/body/checkout/checkout.jsx
+++ Inventory/public/src/components/body/checkout/checkout.jsx
@@ -94,7 +94,7 @@ function Checkout (e) {
           .then((response) => {
             setItems(previous => previous.map((item) => {
               if (item.Item === selectComponent) {
-                return { ...item, Quantity: response.data.Quantity }
+                return { ...item, Quantity: response.data }
               }
               else {
                 return item
