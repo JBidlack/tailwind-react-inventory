acca7f25746131ddcfd59ea404e7d09e0999a07e
JBidlack <john.l.bidlack@gmail.com>
2023-05-01 19:46:53 -0400

    I... I think it works....

--- Inventory/public/src/components/body/empEdit/empEdit.jsx
+++ Inventory/public/src/components/body/empEdit/empEdit.jsx
@@ -30,10 +30,9 @@ const Employees = () => {
         e.preventDefault();
 
         const form = document.querySelector('form');
-        let newName = form.querySelector('input[name=empName]')
-        let newDept = form.querySelector('input[name=dept]')
-        let newEmail = form.querySelector('input[name=email]')
-        let newAdmin = form.querySelector('input[name=admin]')
+        let newName = form.querySelector('input[name=empName]');
+        let newDept = form.querySelector('input[name=dept]');
+        let newEmail = form.querySelector('input[name=email]');
 
         axios.put('/api/employees/' + newName.value + '/new',
         {
@@ -42,7 +41,7 @@ const Employees = () => {
             Email: newEmail.value,
             Admin: isAdmin
         });
-
+        setIsVisible(false);
     };
 
     useEffect(() => {
@@ -101,30 +100,31 @@ const Employees = () => {
                 </table>
             </div>
             <div className={`fixed top-0 left-0 right-0 bottom-0 w-full bg-neutral-500 bg-opacity-30 z-50 flex items-center justify-center h-full ${isVisible ? '' : 'hidden'}`}>
-                <form className='bg-yellow-400 w-1/3 flex justify-center rounded-md shadow-md my-6'>
+                <form className='bg-yellow-400 w-1/3 flex justify-center rounded-lg shadow-md my-6'>
                     <div className='w-full mx-10'>
                         <div className='w-full flex justify-center '>
                             <label className='m-auto flex justify-center font-bold text-xl my-6 mb-8'>Add Employee</label>
                         </div>
-                        <div className='flex justify-start w-full my-4'>
+                        <div className='flex justify-center w-full my-4'>
                             <label className='pr-2'>Name:</label>
-                            <input name='empName' className='w-2/3 rounded-sm shadow-sm' />
+                            <input name='empName' className='w-1/2 rounded-sm shadow-sm' />
                         </div>
-                        <div className='w-full my-4'>
+                        <div className='w-full flex justify-center my-4'>
                             <label className='pr-2'>Department:</label>
-                            <input name='dept' className='w-2/3 rounded-sm shadow-sm' /> 
+                            <input name='dept' className='w-1/2 rounded-sm shadow-sm' /> 
                         </div>
-                        <div className='w-full my-4'>
+                        <div className='flex justify-center w-full my-4'>
                             <label className='pr-2'>E-Mail:</label>
-                            <input name='email' type='email' className='w-2/3 rounded-sm shadow-sm'></input> 
+                            <input name='email' type='email' className='w-1/2 rounded-sm shadow-sm'></input> 
                         </div>
-                        <div className='w-full my-4'>
+                        <div className='flex justify-center w-full my-4'>
                             <label className='pr-2'>Admin:</label>
                             <input name='admin' type='checkbox' className='rounded-sm shadow-sm'
                              onChange={setChecked}/> 
                         </div>
                         <div className='flex justify-center mb-6'>
-                            <button className='border border-black bg-green-600 p-2 rounded-md m-4'>Submit</button>
+                            <button className='border border-black bg-green-600 p-2 rounded-md m-4'
+                            onClick={(e) => addEmp(e)}>Submit</button>
                             <button className='border border-black bg-red-600 p-2 rounded-md m-4'
                             onClick={((e) => handleCancel(e))}>Cancel</button>
                         </div>
