da3f19968e68c07082be9179634ec07595ea7adb
JBidlack <john.l.bidlack@gmail.com>
2023-05-04 18:16:46 -0400

    .

--- Inventory/backend/routes/loginReg.js
+++ Inventory/backend/routes/loginReg.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
