46183a425c167ff9d53dbdbcc5641741eca940ba
JBidlack <john.l.bidlack@gmail.com>
2023-05-04 19:00:26 -0400

    aaaaaa

--- Inventory/backend/index.js
+++ Inventory/backend/index.js
@@ -5,14 +5,13 @@ const  { MongoClient } = require('mongodb');
 const mongodb = require('mongodb');
 const mongoose = require('mongoose');
 const nodemailer = require('nodemailer');
-const router = require('./routes/loginReg.js')
+
 require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/auth', router)
 
 const email = process.env.EMAIL;
 const emailto = process.env.EMAILRECIP;
@@ -45,6 +44,8 @@ invDB.on('connected', () => {
 employeeList.on('connected', () => {
   console.log("We have liftoff employee list!");
 });
+const router = require('./routes/loginReg.js')
+app.use('/auth', router)
 
 // schema
 const invSchema = mongoose.Schema({
